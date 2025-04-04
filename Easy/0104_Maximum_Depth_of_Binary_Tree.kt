6cc8a3bbdb67be2db539363b23c7ccecdfa30f7d
Ayush Padlekar <110286909+ayushpadlekar@users.noreply.github.com>
2025-04-05 00:03:41 +0530

    Create 0104_Maximum_Depth_of_Binary_Tree.kt

---
+++ Easy/0104_Maximum_Depth_of_Binary_Tree.kt
@@ -0,0 +1,24 @@
+// 104. Maximum Depth of Binary Tree
+// Given the root of a binary tree, return its maximum depth.
+// A binary trees maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+// Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 3
+
+// Example 2:
+// Input: root = [1,null,2]
+// Output: 2
+
+// 0ms - Recursive approach - maxOf left & right.
+class Solution {
+    fun maxDepth(root: TreeNode?): Int {
+        
+        if(root == null) return 0
+
+        var leftH = maxDepth(root.left)
+        var rightH = maxDepth(root.right)
+
+        return 1 + maxOf(leftH,rightH) // or - if(leftH > rightH) leftH else rightH
+    }
+}
