d9e72e247910090a91b99f717f0dfdf8d7c07806
Ayush Padlekar <110286909+ayushpadlekar@users.noreply.github.com>
2025-04-28 23:24:21 +0530

    Update 0482_License_Key_Formatting.kt

--- Easy/0482_License_Key_Formatting.kt
+++ Easy/0482_License_Key_Formatting.kt
@@ -7,13 +7,13 @@
 // Furthermore, there must be a dash inserted between two groups, and you should convert all lowercase letters to uppercase.
 // Return the reformatted license key.
 
-// Example 1:
+// Example 1 :
 // Input: s = "5F3Z-2e-9-w", k = 4
 // Output: "5F3Z-2E9W"
 // Explanation: The string s has been split into two parts, each part has 4 characters.
 // Note that the two extra dashes are not needed and can be removed.
 
-// Example 2:
+// Example 2 :
 // Input: s = "2-5g-3-J", k = 2
 // Output: "2-5G-3J"
 // Explanation: The string s has been split into three parts, each part has 2 characters except the first part as it could be shorter as mentioned above.
